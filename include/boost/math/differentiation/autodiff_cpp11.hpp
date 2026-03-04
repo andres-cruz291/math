082759d24f8e14962061534b71e64041d672966d
Andres Cruz <andres.cruz291@gmail.com>
2026-03-04 14:27:57 +0100

    Enable CUDA compatibility for Boost autodiff: define BOOST_MATH_CUDA_ENABLED and apply to 272 functions; add CUDA-compatible std replacements (std_array, cuda_tuple, make_tuple, fill, inner_product, accumulate, factorial, min, for_each, max); include cuda/std/utility; replace std::array and std::* usages in autodiff headers.

--- include/boost/math/differentiation/autodiff_cpp11.hpp
+++ include/boost/math/differentiation/autodiff_cpp11.hpp
@@ -28,32 +28,32 @@ inline namespace autodiff_v1 {
 namespace detail {
 
 template <typename RealType, size_t Order>
-fvar<RealType, Order>::fvar(root_type const& ca, bool const is_variable) {
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>::fvar(root_type const& ca, bool const is_variable) {
   fvar_cpp11(is_fvar<RealType>{}, ca, is_variable);
 }
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-void fvar<RealType, Order>::fvar_cpp11(std::true_type, RootType const& ca, bool const is_variable) {
+BOOST_MATH_CUDA_ENABLED void fvar<RealType, Order>::fvar_cpp11(std::true_type, RootType const& ca, bool const is_variable) {
   v.front() = RealType(ca, is_variable);
   if (0 < Order)
-    std::fill(v.begin() + 1, v.end(), static_cast<RealType>(0));
+    fill(v.begin() + 1, v.end(), static_cast<RealType>(0));
 }
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-void fvar<RealType, Order>::fvar_cpp11(std::false_type, RootType const& ca, bool const is_variable) {
+BOOST_MATH_CUDA_ENABLED void fvar<RealType, Order>::fvar_cpp11(std::false_type, RootType const& ca, bool const is_variable) {
   v.front() = ca;
   if (0 < Order) {
     v[1] = static_cast<root_type>(static_cast<int>(is_variable));
     if (1 < Order)
-      std::fill(v.begin() + 2, v.end(), static_cast<RealType>(0));
+      fill(v.begin() + 2, v.end(), static_cast<RealType>(0));
   }
 }
 
 template <typename RealType, size_t Order>
 template <typename... Orders>
-get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at_cpp11(std::true_type,
+BOOST_MATH_CUDA_ENABLED get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at_cpp11(std::true_type,
                                                                          size_t order,
                                                                          Orders...) const {
   return v.at(order);
@@ -61,7 +61,7 @@ get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at_cpp11(std::tr
 
 template <typename RealType, size_t Order>
 template <typename... Orders>
-get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at_cpp11(std::false_type,
+BOOST_MATH_CUDA_ENABLED get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at_cpp11(std::false_type,
                                                                          size_t order,
                                                                          Orders... orders) const {
   return v.at(order).at(orders...);
@@ -70,29 +70,29 @@ get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at_cpp11(std::fa
 // Can throw "std::out_of_range: array::at: __n (which is 7) >= _Nm (which is 7)"
 template <typename RealType, size_t Order>
 template <typename... Orders>
-get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at(size_t order, Orders... orders) const {
+BOOST_MATH_CUDA_ENABLED get_type_at<RealType, sizeof...(Orders)> fvar<RealType, Order>::at(size_t order, Orders... orders) const {
   return at_cpp11(std::integral_constant<bool, sizeof...(orders) == 0>{}, order, orders...);
 }
 
 template <typename T, typename... Ts>
-constexpr T product(Ts...) {
+BOOST_MATH_CUDA_ENABLED constexpr T product(Ts...) {
   return static_cast<T>(1);
 }
 
 template <typename T, typename... Ts>
-constexpr T product(T factor, Ts... factors) {
+BOOST_MATH_CUDA_ENABLED constexpr T product(T factor, Ts... factors) {
   return factor * product<T>(factors...);
 }
 
 // Can throw "std::out_of_range: array::at: __n (which is 7) >= _Nm (which is 7)"
 template <typename RealType, size_t Order>
 template <typename... Orders>
-get_type_at<fvar<RealType, Order>, sizeof...(Orders)> fvar<RealType, Order>::derivative(
+BOOST_MATH_CUDA_ENABLED get_type_at<fvar<RealType, Order>, sizeof...(Orders)> fvar<RealType, Order>::derivative(
     Orders... orders) const {
   static_assert(sizeof...(Orders) <= depth,
                 "Number of parameters to derivative(...) cannot exceed fvar::depth.");
   return at(static_cast<size_t>(orders)...) *
-         product(boost::math::factorial<root_type>(static_cast<unsigned>(orders))...);
+         product(factorial<root_type>(static_cast<unsigned>(orders))...);
 }
 
 template <typename RootType, typename Func>
@@ -104,7 +104,7 @@ class Curry {
   template <typename SizeType>  // typename SizeType to force inline constructor.
   Curry(Func const& f, SizeType i) : f_(f), i_(static_cast<std::size_t>(i)) {}
   template <typename... Indices>
-  RootType operator()(Indices... indices) const {
+  BOOST_MATH_CUDA_ENABLED RootType operator()(Indices... indices) const {
     using unsigned_t = typename std::make_unsigned<typename std::common_type<Indices>::type...>::type;
     return f_(i_, static_cast<unsigned_t>(indices)...);
   }
@@ -112,7 +112,7 @@ class Curry {
 
 template <typename RealType, size_t Order>
 template <typename Func, typename Fvar, typename... Fvars>
-promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_coefficients(
+BOOST_MATH_CUDA_ENABLED promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_coefficients(
     size_t const order,
     Func const& f,
     Fvar const& cr,
@@ -130,7 +130,7 @@ promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_coef
 
 template <typename RealType, size_t Order>
 template <typename Func, typename Fvar, typename... Fvars>
-promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_coefficients_nonhorner(
+BOOST_MATH_CUDA_ENABLED promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_coefficients_nonhorner(
     size_t const order,
     Func const& f,
     Fvar const& cr,
@@ -156,7 +156,7 @@ promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_coef
 
 template <typename RealType, size_t Order>
 template <typename Func, typename Fvar, typename... Fvars>
-promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_derivatives(
+BOOST_MATH_CUDA_ENABLED promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_derivatives(
     size_t const order,
     Func const& f,
     Fvar const& cr,
@@ -178,7 +178,7 @@ promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_deri
 
 template <typename RealType, size_t Order>
 template <typename Func, typename Fvar, typename... Fvars>
-promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_derivatives_nonhorner(
+BOOST_MATH_CUDA_ENABLED promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_derivatives_nonhorner(
     size_t const order,
     Func const& f,
     Fvar const& cr,
@@ -205,7 +205,7 @@ promote<fvar<RealType, Order>, Fvar, Fvars...> fvar<RealType, Order>::apply_deri
 
 template <typename RealType, size_t Order>
 template <typename SizeType>
-fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::true_type,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::true_type,
                                                                     SizeType z0,
                                                                     size_t isum0,
                                                                     fvar<RealType, Order> const& cr,
@@ -222,7 +222,7 @@ fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::true_ty
 
 template <typename RealType, size_t Order>
 template <typename SizeType>
-fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::false_type,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::false_type,
                                                                     SizeType z0,
                                                                     size_t isum0,
                                                                     fvar<RealType, Order> const& cr,
@@ -235,13 +235,13 @@ fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::false_t
   size_t const i_max = m0 + m1 < Order ? Order - (m0 + m1) : 0;
   fvar<RealType, Order> retval = fvar<RealType, Order>();
   for (size_t i = 0, j = Order; i <= i_max; ++i, --j)
-    retval.v[j] = std::inner_product(
+    retval.v[j] = inner_product(
         v.cbegin() + ssize_t(m0), v.cend() - ssize_t(i + m1), cr.v.crbegin() + ssize_t(i + m0), zero);
   return retval;
 }
 
 template <typename RealType, size_t Order>
-fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply(size_t z0,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply(size_t z0,
                                                               size_t isum0,
                                                               fvar<RealType, Order> const& cr,
                                                               size_t z1,
@@ -251,7 +251,7 @@ fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply(size_t z0,
 
 template <typename RealType, size_t Order>
 template <typename SizeType>
-fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::true_type,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::true_type,
                                                                     SizeType z0,
                                                                     size_t isum0,
                                                                     root_type const& ca) const {
@@ -264,7 +264,7 @@ fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::true_ty
 
 template <typename RealType, size_t Order>
 template <typename SizeType>
-fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::false_type,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::false_type,
                                                                     SizeType z0,
                                                                     size_t isum0,
                                                                     root_type const& ca) const {
@@ -277,7 +277,7 @@ fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply_cpp11(std::false_t
 }
 
 template <typename RealType, size_t Order>
-fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply(size_t z0,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply(size_t z0,
                                                               size_t isum0,
                                                               root_type const& ca) const {
   return epsilon_multiply_cpp11(is_fvar<RealType>{}, z0, isum0, ca);
@@ -285,7 +285,7 @@ fvar<RealType, Order> fvar<RealType, Order>::epsilon_multiply(size_t z0,
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type_cpp11(std::true_type,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type_cpp11(std::true_type,
                                                                                  bool is_root,
                                                                                  RootType const& ca) {
   auto itr = v.begin();
@@ -297,7 +297,7 @@ fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type_cpp11
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type_cpp11(std::false_type,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type_cpp11(std::false_type,
                                                                                  bool is_root,
                                                                                  RootType const& ca) {
   auto itr = v.begin();
@@ -310,57 +310,57 @@ fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type_cpp11
 }
 
 template <typename RealType, size_t Order>
-fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type(bool is_root,
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::multiply_assign_by_root_type(bool is_root,
                                                                            root_type const& ca) {
   return multiply_assign_by_root_type_cpp11(is_fvar<RealType>{}, is_root, ca);
 }
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-fvar<RealType, Order>& fvar<RealType, Order>::negate_cpp11(std::true_type, RootType const&) {
-  std::for_each(v.begin(), v.end(), [](RealType& r) { r.negate(); });
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::negate_cpp11(std::true_type, RootType const&) {
+  for_each(v.begin(), v.end(), [](RealType& r) { r.negate(); });
   return *this;
 }
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-fvar<RealType, Order>& fvar<RealType, Order>::negate_cpp11(std::false_type, RootType const&) {
-  std::for_each(v.begin(), v.end(), [](RealType& a) { a = -a; });
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::negate_cpp11(std::false_type, RootType const&) {
+  for_each(v.begin(), v.end(), [](RealType& a) { a = -a; });
   return *this;
 }
 
 template <typename RealType, size_t Order>
-fvar<RealType, Order>& fvar<RealType, Order>::negate() {
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::negate() {
   return negate_cpp11(is_fvar<RealType>{}, static_cast<root_type>(*this));
 }
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-fvar<RealType, Order>& fvar<RealType, Order>::set_root_cpp11(std::true_type, RootType const& root) {
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::set_root_cpp11(std::true_type, RootType const& root) {
   v.front().set_root(root);
   return *this;
 }
 
 template <typename RealType, size_t Order>
 template <typename RootType>
-fvar<RealType, Order>& fvar<RealType, Order>::set_root_cpp11(std::false_type, RootType const& root) {
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::set_root_cpp11(std::false_type, RootType const& root) {
   v.front() = root;
   return *this;
 }
 
 template <typename RealType, size_t Order>
-fvar<RealType, Order>& fvar<RealType, Order>::set_root(root_type const& root) {
+BOOST_MATH_CUDA_ENABLED fvar<RealType, Order>& fvar<RealType, Order>::set_root(root_type const& root) {
   return set_root_cpp11(is_fvar<RealType>{}, root);
 }
 
 template <typename RealType, size_t Order, size_t... Is>
-auto make_fvar_for_tuple(mp11::index_sequence<Is...>, RealType const& ca)
+BOOST_MATH_CUDA_ENABLED auto make_fvar_for_tuple(mp11::index_sequence<Is...>, RealType const& ca)
     -> decltype(make_fvar<RealType, zero<Is>::value..., Order>(ca)) {
   return make_fvar<RealType, zero<Is>::value..., Order>(ca);
 }
 
 template <typename RealType, size_t... Orders, size_t... Is, typename... RealTypes>
-auto make_ftuple_impl(mp11::index_sequence<Is...>, RealTypes const&... ca)
+BOOST_MATH_CUDA_ENABLED auto make_ftuple_impl(mp11::index_sequence<Is...>, RealTypes const&... ca)
     -> decltype(std::make_tuple(make_fvar_for_tuple<RealType, Orders>(mp11::make_index_sequence<Is>{},
                                                                       ca)...)) {
   return std::make_tuple(make_fvar_for_tuple<RealType, Orders>(mp11::make_index_sequence<Is>{}, ca)...);
@@ -369,7 +369,7 @@ auto make_ftuple_impl(mp11::index_sequence<Is...>, RealTypes const&... ca)
 }  // namespace detail
 
 template <typename RealType, size_t... Orders, typename... RealTypes>
-auto make_ftuple(RealTypes const&... ca)
+BOOST_MATH_CUDA_ENABLED auto make_ftuple(RealTypes const&... ca)
     -> decltype(detail::make_ftuple_impl<RealType, Orders...>(mp11::index_sequence_for<RealTypes...>{},
                                                               ca...)) {
   static_assert(sizeof...(Orders) == sizeof...(RealTypes),
--- include/boost/math/tools/config.hpp
+++ include/boost/math/tools/config.hpp
@@ -481,6 +481,17 @@ namespace boost{ namespace math{
 #define BOOST_MATH_CONSTEXPR_TABLE_FUNCTION
 #endif
 
+//
+// CUDA support:
+//
+
+#ifdef __CUDACC__
+#  define BOOST_MATH_CUDA_ENABLED __host__ __device__
+#endif
+
+#ifndef BOOST_MATH_CUDA_ENABLED
+#  define BOOST_MATH_CUDA_ENABLED
+#endif
 
 #endif // BOOST_MATH_TOOLS_CONFIG_HPP
 
